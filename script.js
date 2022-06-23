a84c66c363807e5c68ba263792ec849cb7f10fdb
dominique <dominiquemcc3@gmail.com>
2022-06-23 15:55:16 -0400

    UpdateWebsite

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let stars = $("#stars");
+let moon = $("#moon");
+let mountains_behind = $("#mountains_behind");
+let text = $("#text");
+let mountains_front = $("#mountains_front");
+$(window).on("scroll", function () {
+  let targetEl = $(".section_parallax1-content");
+  let posY = targetEl[0].getBoundingClientRect().y;
+  let value = Math.abs(posY);
+  if (value > targetEl[0].clientHeight || posY > 0) return false;
+  stars.css({
+    left: value * 0.25 + "px",
+  });
+  moon.css({
+    top: value * 1.05 + "px",
+  });
+  mountains_behind.css({
+    top: value * 0.5 + "px",
+  });
+  mountains_front.css({
+    top: value * 0 + "px",
+  });
+  text.css({
+    marginRight: value * 4 + "px",
+    marginTop: value * 1.5 + "px",
+  });
+});
+
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
